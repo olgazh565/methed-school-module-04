c3b656c70fb090b1abd4d41c21cce77230bc9364
olgazh565 <olgazh565@gmail.com>
2023-05-10 21:08:22 +0300

    task complete

---
+++ assets/scripts/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const productName = 'book';
+const productAmount = 20;
+const productCategory = 'education';
+const productPrice = 300;
+
+console.log(productName);
+
+const productTotal = productAmount * productPrice;
+
+console.log(productTotal + ' рублей');
