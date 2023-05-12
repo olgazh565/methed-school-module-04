cf40792bdc997af7a8665f49dd4d4a76ca5a8e7f
olgazh565 <olgazh565@gmail.com>
2023-05-12 22:44:01 +0300

    task complete

---
+++ assets/scripts/einstein.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const catWeight = 6;
+const speedLight = 3e8;
+const energy = catWeight * speedLight ** 2;
+
+console.log(`Энергия тела кота: ${energy} джоулей`);
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,14 @@
 'use strict';
 
-const productName = 'книга';
-const productAmount = 20;
-const productCategory = 'образование';
-const productPrice = 300;
-
-console.log('Наименование товара: ' + productName);
-
+const productName = prompt('Введите наименование товара', "");
+const productAmount = +prompt('Введите количество товара', "");
+const productCategory = prompt('Введите категорию товара', "");
+const productPrice = +prompt('Введите цену товара', "");
 const productTotal = productAmount * productPrice;
 
-console.log('Общая сумма товара: ' + productTotal + ' рублей');
+
+console.log('тип данных productName: ' + typeof productName);
+console.log('тип данных productAmount: ' + typeof productAmount);
+console.log('тип данных productCategory: ' + typeof productCategory);
+console.log('тип данных productPrice: ' + typeof productPrice);
+console.log(`На складе ${productAmount} единицы товара "${productName}" на сумму ${productTotal} деревянных`);
