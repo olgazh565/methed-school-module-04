d9096164ce3bc48412df6eb8fd882f8226766e97
olgazh565 <olgazh565@gmail.com>
2023-05-13 20:28:45 +0300

    task complete

--- assets/scripts/tasksHard_1.js
+++ assets/scripts/tasksHard_1.js
@@ -2,22 +2,22 @@
 
 // Задача 1
 
-// const income = +prompt('Cумма вашего дохода?');
-// let taxSum;
+const income = +prompt('Cумма вашего дохода?');
+let taxSum;
 
-// switch (true) {
-//     case (income < 15000):
-//         taxSum = income * 0.13
-//         break;
-//     case (15000 <= income && income < 50000):
-//         taxSum = income * 0.2
-//         break;
-//     case (income >= 50000):
-//         taxSum = income * 0.3
-//         break;
-//     default:
-//         break;
-// }
+switch (true) {
+    case (income < 15000):
+        taxSum = income * 0.13
+        break;
+    case (15000 <= income && income < 50000):
+        taxSum = income * 0.2
+        break;
+    case (income >= 50000):
+        taxSum = income * 0.3
+        break;
+    default:
+        break;
+}
 
-// console.log('Сумма налога: ' +  Math.round(taxSum));
+console.log('Сумма налога: ' +  Math.round(taxSum));
 
