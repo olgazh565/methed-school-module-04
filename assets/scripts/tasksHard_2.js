faf5bf2e1f29a7b0221abe61ef5f2db11a1dd4c0
olgazh565 <olgazh565@gmail.com>
2023-05-13 13:09:02 +0300

    task complete

---
+++ assets/scripts/tasksHard_2.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Задача 2
+
+// const income2 = +prompt('Укажите сумму вашего дохода?');
+// let taxSum2;
+// const sumLimitFirst = 14999;
+// const sumLimitSecond = 50000;
+
+// switch (true) {
+//     case (income2 < 15000):
+//         taxSum2 = income2 * 0.13
+//         break;
+//     case (15000 <= income2 && income2 < 50000):
+//         taxSum2 = (income2 - sumLimitFirst) * 0.2 + sumLimitFirst * 0.13 
+//         break;
+//     case (income2 >= 50000):
+//         taxSum2 = (income2 - sumLimitSecond) * 0.3 + (sumLimitSecond - sumLimitFirst) * 0.2 + sumLimitFirst * 0.13
+//         break;
+//     default:
+//         break;
+// }
+
+// console.log('Сумма налога: ' + Math.round(taxSum2));
