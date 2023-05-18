4a2e33cbf36579f946be4b6709950e3b7cf9e00a
olgazh565 <olgazh565@gmail.com>
2023-05-19 00:39:34 +0300

    task complete

--- assets/scripts/tasksHard_2.js
+++ assets/scripts/tasksHard_2.js
@@ -2,33 +2,33 @@
 
 // Задача 2
 
-const income2 = +prompt('Укажите сумму вашего дохода?');
-let taxSum2;
+const income = +prompt('Укажите сумму вашего дохода?');
+let taxSum;
 let sumLeft;
 
-if (income2 >= 50000) {
-    taxSum2 = (income2 - 50000) * 0.3;
+if (income >= 50000) {
+    taxSum = (income - 50000) * 0.3;
     sumLeft = 50000;
 }
 
 if (sumLeft) {
-    taxSum2 += (sumLeft - 15000) * 0.2;
+    taxSum += (sumLeft - 15000) * 0.2;
     sumLeft = 15000;
-} else if (income2 > 15000 && income2 < 50000) {
-    taxSum2 = (income2 - 15000) * 0.2;
+} else if (income > 15000 && income < 50000) {
+    taxSum = (income - 15000) * 0.2;
     sumLeft = 15000;
 } 
 
 if (sumLeft) {
-    taxSum2 += sumLeft * 0.13;
+    taxSum += sumLeft * 0.13;
     sumLeft = 0;
-} else if (income2 > 0 && income2 < 15000) {
-    taxSum2 = income2 * 0.13;
+} else if (income > 0 && income < 15000) {
+    taxSum = income * 0.13;
     sumLeft = 0;
 } else {
     alert('Сумма дохода для расчета должна быть больше 0!')
 }
 
-console.log('Сумма налога: ' + taxSum2);
+console.log('Сумма налога: ' + taxSum);
 
 
