6d11465de131fa2ba49d1065b899d24e700eb9f4
olgazh565 <olgazh565@gmail.com>
2023-05-20 16:57:52 +0300

    task complete

--- assets/scripts/task02.js
+++ assets/scripts/task02.js
@@ -4,7 +4,7 @@ const allCashbox = [4500, 3210, 650, 1250, 7830, 990, 13900, 370];
 
 const getAverageValue = (arr) => {
     
-    return Math.floor(arr.reduce((sum, value) => sum + value) / arr.length);
+    return Math.floor(arr.reduce((sum, value) => sum + value, 0) / arr.length);
 }
 
 console.log(getAverageValue(allCashbox));
